20a880279bfcb7e67f87dcce843aedd1c2b52d08
ManurangaJay <manurangajayawardhana@gmail.com>
2024-07-20 20:57:27 +0530

    Functions for View User were created

--- client_app/src/layout/Navbar.js
+++ client_app/src/layout/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     <div>
         <nav class="navbar bg-primary" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand">User Management Application</a>
+                <Link className="navbar-brand" to='/'>User Management Application</Link>
                 <form className="d-flex" role="search">
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                     <button className="btn btn-outline-light" type="submit">Search</button>

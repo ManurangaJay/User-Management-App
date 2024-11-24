706586833b327f6672e0bf6fed6d4a78e98bcb8f
ManurangaJay <manurangajayawardhana@gmail.com>
2024-11-24 23:20:57 +0530

    Readme file was added

--- src/main/java/com/manuJay/userManagementSystem/user/UserController.java
+++ src/main/java/com/manuJay/userManagementSystem/user/UserController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:3001")
 public class UserController {
     private UserService userService;
 
